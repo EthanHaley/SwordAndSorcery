0c5e8a7702f6c3a0fdbc16a5c5fe0e0497154a56
rinikercody <rinikercody@gmail.com>
2017-12-01 14:56:33 -0600

    Tested Queries

--- queries_pt2.sql
+++ queries_pt2.sql
@@ -1,30 +1,29 @@
 DROP FUNCTION IF EXISTS GetPartyExp(p_id int); 
-DROP FUNCTION IF EXISTS GetAverageExp(p_id int);
+DROP FUNCTION IF EXISTS GetAverageKills(parts bigint, kills bigint);
 
 --Calculater Average monster kills for party
-CREATE OR REPLACE FUNCTION GetAverageExp(p_id int)
-RETURNS numeric AS $$
+CREATE OR REPLACE FUNCTION GetAverageKills(parts bigint, kills bigint)
+RETURNS int AS $$
 BEGIN
-SELECT avg(*) FROM(
-SELECT sum(*) FROM encounters
-GROUP BY monster_deaths
-) AS x;
+return kills/parts;
 END; $$
 LANGUAGE PLPGSQL;
 
 --Get average monster kills of each party for ranking.
-SELECT * FROM parties
-ORDER BY GetAverageExp(parties.id);
+SELECT * FROM encounters
+WHERE encounters.monster_deaths > GetAverageKills(
+(SELECT count(parties.id) FROM parties)
+,(SELECT sum(monster_deaths) FROM encounters));
 
 -- Gets all charaters of a certian level who know a certian spell.
 SELECT * FROM characters
 JOIN Spells_Known ON spells_known.character_id = characters.id
-WHERE characters._level = '?' AND spells_known.spell_id = 1;  
+WHERE characters._level = '1' AND spells_known.spell_id = 1;  
 
 -- Gets parties where there is at least one characters of certian class.
 SELECT * FROM parties
 JOIN characters ON parties.id = characters.party_id
-WHERE Characters._class = '?'; 
+WHERE characters._class = 'Ranger'; 
 
 -- Get all parties and orders them by monster deaths
 SELECT * FROM encounters
@@ -32,21 +31,16 @@ ORDER BY party_id, monster_id, monster_deaths;
 
 -- Gets all weapons of a certian damage type
 SELECT * FROM weapons
-WHERE damage_type LIKE '%' || '?' || '%'; 
+WHERE damage_type LIKE '%' || 'Fire' || '%'; 
 
 -- Gets total characters of certian race.
-SELECT count(*) FROM characters
+SELECT characters.race, count(*) FROM characters
 GROUP BY characters.race;
 
 -- Get total characters of certain class.
-SELECT count(*) FROM characters
+SELECT characters._class, count(*) FROM characters
 GROUP BY characters._class;
 
--- Gets all Weapons in characters inventory that are above average in damage. This can be used as template for similar queries
-SELECT items.name, weapons.damage_die FROM characters
-JOIN inventory ON characters.id = inventory.character_id
-JOIN items ON inventory.item_id = items.id
-JOIN weapons ON items.id = weapons.id
-WHERE (weapons.damage_die::numeric) > (
-     SELECT AVG(damage_die::numeric) FROM weapons
-);   
+-- Find specific Armor bonus
+SELECT * FROM armor
+WHERE bonus = 'Heavy'; 
